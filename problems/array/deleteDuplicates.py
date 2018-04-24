34d534d36a2f08c68e364da12e592fe6342ae997
ApolloMat <apollomat@umich.edu>
2018-04-23 22:48:04 -0500

    deleteDuplicates

---
+++ problems/array/deleteDuplicates.py
@@ -0,0 +1,17 @@
+# input: sorted array with duplicates
+
+def remove(a):
+
+    start = 0
+    for indx in range(len(a)):
+        if a[indx] == a[start]:
+            continue
+        else:
+            start += 1
+            a[start] = a[indx]
+    return start + 1
+
+if __name__ == "__main__":
+    a = [1,1,1,1,2,2,2,3,4,4,4,4,5,6,6,6]
+    end = remove(a)
+    print a[:end]
